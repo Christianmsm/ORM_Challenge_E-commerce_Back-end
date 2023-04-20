c5a68c6c47859d62a0b78f636b8714b2d066caba
Christian Smith <christiansmith@Christians-MBP.attlocal.net>
2023-04-20 13:19:53 -0400

    updated server.js file to run sequelize once the application starts + adjusted models index.js file

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,24 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const Product = require('./Product.js');
+const Tag = require('./Tag.js');
 
 class Category extends Model {}
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true, 
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
@@ -16,5 +28,7 @@ Category.init(
     modelName: 'category',
   }
 );
+
+
 
 module.exports = Category;
---
+++ server.js
@@ -0,0 +1,17 @@
+//Packages needed for the application
+const express = require('express');
+const Sequelize = require('sequelize');
+
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//middleware for the application
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Connection to the database
+sequelize.sync().then(() => {
+    app.listen(PORT, () => console.log('Server now listening'));
+});
