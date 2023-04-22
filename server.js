a577be03aa739e1adfeaa2f7417da294dd80628e
Christian Smith <christiansmith@Christians-MBP.attlocal.net>
2023-04-21 21:20:24 -0400

    connected the different tables together with belongsTo & belongsToMany methods

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// app.use(apiRoutes);
+
 //Connection to the database
 sequelize.sync().then(() => {
     app.listen(PORT, () => console.log('Server now listening'));
