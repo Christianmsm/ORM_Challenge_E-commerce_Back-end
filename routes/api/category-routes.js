60d02f6b7ad77120896ae0f4cceeaeec4df0918d
Christian Smith <christiansmith@Christians-MBP.attlocal.net>
2023-04-21 21:45:02 -0400

    filled in most of the routes for the category-routes file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,16 +35,41 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+//This block is waiting to be initialized to create a new category, then once that happens it request the body of the category class then send the information in json. If it doesnt work it'll send an error to the client
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      //need to add to this
+    }
+  )
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  //This block waits to be initialized, once that happens it will delete a category by its id value. If that id doesn't exist it will five an error saying it doesnt exist. If it does, then it will delete the category.
+  try {
+    const categoryData = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
